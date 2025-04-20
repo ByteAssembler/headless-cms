69f53b988c1cc1f855a367e4ed17edbc26c16214
ByteAssembler <65082118+ByteAssembler@users.noreply.github.com>
2025-04-20 22:17:03 +0200

    feat: integrate drizzle ORM with SQLite and set up user schema
    - Added dotenv configuration for environment variables.
    - Established a connection to the SQLite database using drizzle ORM.
    - Created a new drizzle configuration file for database setup.
    - Defined a user schema with fields for id, name, age, and email.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+
+const client = createClient({ url: process.env.DB_FILE_NAME! });
+const db = drizzle({ client });
+
 const app = new Hono()
 
 app.get('/', (c) => {
